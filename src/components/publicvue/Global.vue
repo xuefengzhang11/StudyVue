6b3adf2a1cf2878ff5b62e28228735d6b622c743
sx-hyy <sx_hyy@163.com>
2018-11-06 13:32:00 +0800

    hyy 2018-11-6

--- src/components/publicvue/Global.vue
+++ src/components/publicvue/Global.vue
@@ -5,8 +5,8 @@
 <script>
 export default {
   // 后端服务器主机号
-  HOST: 'http://127.0.0.1:8000/',
-  // HOST: '47.107.54.146:8000/',
+  // HOST: 'http://127.0.0.1:8000/',
+  HOST: 'http://47.107.54.146:8000/',
   // 图片服务器主机号（七牛云）
   IMG: 'http://pgu05jbff.bkt.clouddn.com/'
 }
